b4c09d0b7c538e5f8b57d2fc5be50b06167c88b2
jdornbusch <joachim.dornbusch@crdp.ac-versailles.fr>
2013-06-27 16:23:11 +0200

    added scripts for video js

--- video/scripts/preview.js
+++ video/scripts/preview.js
@@ -6,14 +6,15 @@ function reactivatePreview(idPreview) {
 	player.play();
 };
 function activatePreview(idPreview) {
-	createStyleSheetLinkTag("video-js.min.css");
+	videojs.options.flash.swf = "http://apiscol.crdp-versailles.fr/cdn/video-js/video-js.swf" 
+	createStyleSheetLinkTag("video-js/video-js.min.css");
 	$video = $("#" + idPreview);
 	var videoURL = $video.attr("data-url");
 	$previewArea = $video.closest(".preview-area");
 	$video.replaceWith('<video id="' + idPreview + '"'
 			+ 'class="video-js vjs-default-skin"'
 			+ 'controls preload="auto" width="640" height="264"'
-			+ 'poster="http://video-js.zencoder.com/oceans-clip.png"'
+			+ 'poster="http://apiscol.crdp-versailles.fr/cdn/commons/img/logo-api.png"'
 			+ 'data-setup=\'{"example_option":true}\'>' + '<source src="'
 			+ videoURL + '/output.mp4" type="video/mp4" />' + '<source src="'
 			+ videoURL + '/output.ogg" type="video/ogg" />' + '</video>')
