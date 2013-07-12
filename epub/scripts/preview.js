d6352b0ad5461a2b5463247c85abf944658f74c9
jdornbusch <joachim.dornbusch@crdp.ac-versailles.fr>
2013-07-12 13:23:48 +0200

    fixed monocle book data object img path problems

--- epub/scripts/preview.js
+++ epub/scripts/preview.js
@@ -8,8 +8,22 @@ function activatePreview(idPreview) {
 	$previewArea = $epubPreview.closest(".preview-area");
 	$epubPreview.show();
 	$epubPreview.find("monocle-")
-	Monocle.Reader( idPreview.replace("preview", "monocle"), bookData, {}, function(reader) {
-		
+	Monocle.Reader( idPreview.replace("preview", "monocle"), bookData, {}, function(rdr) {
+		 window.reader2 = rdr;
+         var magnifier = new Monocle.Controls.Magnifier(rdr);
+         rdr.addControl(magnifier);
+         var scrubber = new Monocle.Controls.Scrubber(rdr);
+         rdr.addControl(scrubber);
+         var toc = Monocle.Controls.Contents(rdr);
+         rdr.addControl(toc, 'popover', { hidden: true });
+         createBookTitle(
+           rdr,
+           {
+             start: function () {
+               rdr.showControl(toc);
+             }
+           }
+         );
 	});
 	$previewArea.addClass("activated");
 }
